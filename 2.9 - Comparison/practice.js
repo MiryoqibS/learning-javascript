4935b8bf53fa0334fb49cc3e00077f18f1b0761a
Miryoqib Sobirov <miryoqibcoder@gmail.com>
2025-04-22 13:48:46 +0500

    2.9 - Операторы сравнения

---
+++ 2.9 - Comparison/practice.js
@@ -0,0 +1,12 @@
+/*
+== Задание 1 с сайта ==
+Каким будет результат этих выражений?
+
+5 > 4 = true
+"ананас" > "яблоко" = false
+"2" > "12" = true
+undefined == null = true
+undefined === null = false
+null == "\n0\n" = false
+null === +"\n0\n" = false
+*/
